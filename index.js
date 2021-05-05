d0a19302ea7d20bca1415d6cc227b30a1cf5a68f
Quentin Chauvat <quentin.chauvat@gmail.com>
2021-05-05 16:49:41 +0200

    updated lab

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { model } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -21,7 +22,36 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+   
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  const firstRecipe = {
+    title: "pate au pesto",
+     level: "Easy Peasy",
+    "ingredients": [
+    "pasta",
+    "cheese",
+  ],
+  "cuisine": "Italian",
+  "dishType": "main_course",
+  "image": "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
+  "duration": 20,
+  "creator": "Chef Peperroni"
+  ,};
+  
+  Recipe.create(firstRecipe)
+  .then (recipe =>console.log("title", recipe))  
+  .catch (error=> console.log("An error occurred, receipes are not imported", error));
+
+  Recipe.insertMany(data)
+  .then(recipe => console.log("All recipes are imported", recipe))
+  .catch(error=> console.log("An error occurred, receipes are not imported", error));
+
+  Recipe.findByIdAndUpdate('6092aecf46f650060068fa48')
+    .then(recipe => {recipe.duration = 100;
+      return recipe.save();
+    })
+    .catch(error => console.log ("Recipe has not been updated", error));
